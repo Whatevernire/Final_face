368354b7061a2b002f0548412a65ba8ce79a793a
Viktor <33853938+Whatevernire@users.noreply.github.com>
2019-02-18 15:11:48 +0900

    Update Cam.client
    Надо позже почекать на работоспособность

--- Detection.py
+++ Detection.py
@@ -1,66 +1,55 @@
-#!/usr/bin/env python
+import cv2
 from PIL import Image
 import face_recognition
-import os
-import cv2
-import sqlite3
-import pika
-
-video_capture = cv2.VideoCapture(0)
-face_locations = []
+import time
+import numpy as np
+import socket 
 
 
-parameters = pika.URLParameters('amqp://admin2:123@185.185.68.195:5672')
-connection = pika.BlockingConnection(parameters)
-channel = connection.channel()
+faaace = []
+faace_res = []
+# multiple cascades: https://github.com/Itseez/opencv/tree/master/data/haarcascades
+faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+cap = cv2.VideoCapture(0)
 
 
-channel.queue_declare(queue='face')
-channel.queue_declare(queue='width')
-channel.queue_declare(queue='hight')
+TCP_IP = '127.0.0.1' # это ip raspberry
+TCP_PORT = 5005 
+BUFFER_SIZE = 1024  
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+s.connect((TCP_IP, TCP_PORT))
 
 
-i=0
-def main():
-    while True:
 
-        # Взятие кадра с видео
-        ret, frame = video_capture.read()
+while True:
+    ret, img = cap.read()
+    start = time.time()
 
-        # Уменьшение кадра до 1/4 для быстрого распознования лица
-        small_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(
+        gray,
 
-        # Конвертация изображения из BGR color (which OpenCV uses) в RGB color (с которым работает библиотека)
-        rgb_small_frame = small_frame[:, :, ::-1]
-        face_locations = face_recognition.face_locations(rgb_small_frame)
-        face_locations = face_recognition.face_locations(rgb_small_frame, number_of_times_to_upsample=0)
-        if face_locations != 0:
-            for face_location in face_locations:
-                top, right, bottom, left = face_location
-                face_image = rgb_small_frame[top - 10:bottom + 10, left - 10:right + 10]
-                #Создаем обрезанные фото и берем с них размеры окна
-                pil_image = Image.fromarray(face_image)
-                hight = pil_image.size[0]
-                weight = pil_image.size[1]
-                pil_image = pil_image.tobytes()
-                i= i+1
-                #Вносим значения в виде: bytes, int, int, самого фото и его размеры.
-                channel.basic_publish(exchange='',
-                                      routing_key='face',
-                                      body=pil_image)
-                hight = hight.encode()
-                channel.basic_publish(exchange='',
-                                      routing_key='width',
-                                      body=hight)
-                weight = weight.encode()
-                channel.basic_publish(exchange='',
-                                      routing_key='hight',
-                                      body=weight)
-                print(" [x] Sent ")
-                connection.close()
-                # Вырубаем на четвертом круге.
-                if i >= 3:
-                    i=0
-                    break
+        scaleFactor=1.2,
+        minNeighbors=10,
+        minSize=(20, 20)
+    )
 
-        print('Is Ok')
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x-10, y-10), (x+10 + w, y+10 + h), (255, 0, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = img[y:y + h, x:x + w]
+        box = (x+20,y+20,x+20+w,y+20+h)
+        face_image= img
+        face_image = Image.fromarray(face_image,'RGB')
+        face_image=face_image.crop(box)
+        MESSAGE = face_image      
+        s.send(MESSAGE) 
+        data = s.recv(BUFFER_SIZE) 
+
+        print(start - time.time())
+
+
+
+cap.release()
+cv2.destroyAllWindows()
